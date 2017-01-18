115ee3204f57e5846a28b60a8710cff8e0ff5e0c
MiquelQ <miquelquetglas1986@gmail.com>
2017-01-18 19:18:46 +0100

    búsqueda

--- client/web/lib/component/home/home_component.dart
+++ client/web/lib/component/home/home_component.dart
@@ -1,6 +1,6 @@
 import 'package:angular2/core.dart';
 //import 'package:angular2/router.dart';
-import 'json_object/json_object.dart';
+import 'package:json_object/json_object.dart';
 import 'dart:html';
 import 'dart:convert';
 @Component(
@@ -16,22 +16,49 @@ class HomeComponent{
       //print('bText: ${bText.value}');
       window.location.assign('http://opendata.aragon.es/catalogo/catalogo.html?q=${bText.value}');    
     }  
-    Map autocompletar;
-    onKey(dynamic event) {
- 		//print(event.target.value);
- 		var url = "http://opendata.aragon.es/catalogo/api/2/util/dataset/autocomplete?incomplete=%${event.target.value}%";
+    List autocompletar;
+    String jsonAuto;
+    int lol = 0;
+    String valor;
 
+    onKey(dynamic event) {
+ 		
+		//print(event.target.value);
+		var url = "http://opendata.aragon.es/catalogo/api/2/util/dataset/autocomplete?incomplete=%${event.target.value}%";
+		var request = HttpRequest.getString(url);
+		valor = event.target.value;
+		request.then(
+			(String Result) {
+				Map decoded = JSON.decode(Result);
+				if(valor != "") {
+					autocompletar = decoded['ResultSet']['Result'];
+				}
+				else{
+					//le devolvemos json vacío
+					//autocompletar= decoded['ResultSet']['no'];
+					autocompletar=null;
+				}
+				print(autocompletar);
+			}
+		);
+	}
+
+/*
+ 		//var url = "http://opendata.aragon.es/catalogo/api/2/util/dataset/autocomplete?incomplete=%test%";
   		// call the web server asynchronously
   		var request = HttpRequest.getString(url).then(
   			(Result){
-  					JsonObject cadena = new JsonObject.fromJsonString(Result);
-  					print(cadena);
-  					//autocompletar = JSON.decode(Result);	
-  					//print(autocompletar);
-  					//if(autocompletar is Map){print('Es un Mapa');}
-
+  					jsonAuto = JSON.decode(Result);
+  					lol = lol +1;
+  					print(lol);
+  					//print(cadena);	
+  					autocompletar = jsonAuto['ResultSet']['Result'];
+  					print(autocompletar);
   				}
-
   			);
- 	}
+
+ 		}*/
+
+
 }
+
