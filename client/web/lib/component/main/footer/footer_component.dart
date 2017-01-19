f5448a3963eff88a2fd10dc84ea1b07ad83ed196
zarisi <isdswii@gmail.com>
2017-01-19 10:46:49 +0100

    small import fixes

--- client/web/lib/component/main/footer/footer_component.dart
+++ client/web/lib/component/main/footer/footer_component.dart
@@ -2,11 +2,8 @@ import 'package:angular2/core.dart';
 import 'package:angular2_components/angular2_components.dart';
 //import 'package:angular2/router.dart';
 
-
 @Component(
-    selector: 'footer',
-    templateUrl: 'footer.html',
-    )
-
-class FooterComponent {
-}
+  selector: 'footer',
+  templateUrl: 'footer.html',
+)
+class FooterComponent {}
