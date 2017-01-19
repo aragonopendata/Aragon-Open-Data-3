1e812387461b5332c47fce229edd1bc6c0f3f1e4
zarisi <isdswii@gmail.com>
2017-01-19 09:49:13 +0100

    Added search
    Added the ability of search datasets from the home

--- client/web/lib/component/home/home_component.dart
+++ client/web/lib/component/home/home_component.dart
@@ -1,64 +1,36 @@
 import 'package:angular2/core.dart';
 //import 'package:angular2/router.dart';
-import 'package:json_object/json_object.dart';
+//import 'package:json_object/json_object.dart';
 import 'dart:html';
 import 'dart:convert';
+
 @Component(
-    selector: 'home',
-    templateUrl: 'home.html',
-    )
+  selector: 'home',
+  templateUrl: 'home.html',
+)
+class HomeComponent {
+  void goCkan(Event e) {
+    e.preventDefault();
+    InputElement bText = querySelector('[name="buscador_home"]');
+    //print('bText: ${bText.value}');
+    window.location.assign(
+        'http://opendata.aragon.es/catalogo/catalogo.html?q=${bText.value}');
+  }
 
-class HomeComponent{
- 	
-    void goCkan(Event e) {
-      e.preventDefault();
-      InputElement bText = querySelector('[name="buscador_home"]');
-      //print('bText: ${bText.value}');
-      window.location.assign('http://opendata.aragon.es/catalogo/catalogo.html?q=${bText.value}');    
-    }  
-    List autocompletar;
-    String jsonAuto;
-    int lol = 0;
-    String valor;
+  List autocompletar;
+  String valor;
 
-    onKey(dynamic event) {
- 		
-		//print(event.target.value);
-		var url = "http://opendata.aragon.es/catalogo/api/2/util/dataset/autocomplete?incomplete=%${event.target.value}%";
-		var request = HttpRequest.getString(url);
-		valor = event.target.value;
-		request.then(
-			(String Result) {
-				Map decoded = JSON.decode(Result);
-				if(valor != "") {
-					autocompletar = decoded['ResultSet']['Result'];
-				}
-				else{
-					//le devolvemos json vacío
-					//autocompletar= decoded['ResultSet']['no'];
-					autocompletar=null;
-				}
-				print(autocompletar);
-			}
-		);
-	}
-
-/*
- 		//var url = "http://opendata.aragon.es/catalogo/api/2/util/dataset/autocomplete?incomplete=%test%";
-  		// call the web server asynchronously
-  		var request = HttpRequest.getString(url).then(
-  			(Result){
-  					jsonAuto = JSON.decode(Result);
-  					lol = lol +1;
-  					print(lol);
-  					//print(cadena);	
-  					autocompletar = jsonAuto['ResultSet']['Result'];
-  					print(autocompletar);
-  				}
-  			);
-
- 		}*/
-
-
+  onKey(dynamic event) {
+    String url =
+        "http://opendata.aragon.es/catalogo/api/2/util/dataset/autocomplete?incomplete=%${event.target.value}%";
+    valor = event.target.value;
+    HttpRequest.request(url).then((HttpRequest result) {
+      Map decoded = JSON.decode(result.response);
+      if (valor != "") {
+        autocompletar = decoded['ResultSet']['Result'];
+      } else {
+        autocompletar = null;
+      }
+    })..catchError((error) => print("ERROR: couldnt get a response from the server: $error"));
+  }
 }
-
