be6842c2ae75510ef21b762ee987dc2ead70f76a
zarisi <isdswii@gmail.com>
2017-01-20 13:38:38 +0100

    Added popups

--- client/web/lib/component/info/aplicaciones/aplicaciones_component.dart
+++ client/web/lib/component/info/aplicaciones/aplicaciones_component.dart
@@ -1,19 +1,43 @@
+import 'dart:html';
 import 'package:angular2/core.dart';
 import 'package:aod3/service/info_service.dart';
+import 'package:angular2_components/angular2_components.dart';
 
 @Component(
     selector: 'aplicaciones',
     templateUrl: 'aplicaciones.html',
-    providers: const [InfoService]
+    directives: const [materialDirectives, defaultPopupSizeProvider],
+    providers: const [materialProviders,InfoService]
 )
 
 class AplicacionesComponent implements OnInit{
   final InfoService _infoService;
   AplicacionesComponent(this._infoService);
+  bool showMaxHeightDialog = false;
+  String redirectUrl;
 
   List aplicaciones;
   @override
   ngOnInit() {
     aplicaciones = _infoService.aplicaciones;
   }
+
+  redirect(MouseEvent event,String url){
+    showMaxHeightDialog = true;
+    redirectUrl = url;
+  }
+
+  redirectToNewPage(){
+    window.window.location.href = redirectUrl;
+  }
 }
+
+@Injectable()
+PopupSizeProvider createPopupSizeProvider() {
+  return const PercentagePopupSizeProvider();
+}
+
+@Directive(selector: '[defaultPopupSizeProvider]', providers: const [
+  const Provider(PopupSizeProvider, useFactory: createPopupSizeProvider)
+])
+class defaultPopupSizeProvider {}
