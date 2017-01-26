049c68a2daa8d19a4b33e6a5d3a4ce32a9894f1d
zarisi <isdswii@gmail.com>
2017-01-26 09:49:34 +0100

    Added campus

--- client/web/lib/component/info/campus/campus_component.dart
+++ client/web/lib/component/info/campus/campus_component.dart
@@ -4,17 +4,26 @@ import 'package:aod3/service/campus_service.dart';
 @Component(
     selector: 'campus',
     templateUrl: 'campus.html',
+    directives: const [],
     providers: const [CampusService])
 class CampusComponent implements OnInit{
   final CampusService _campusService;
   CampusComponent(this._campusService);
   List<Map> campusList;
+  List<Map> tiposList;
+  int tipoValue;
+
+  prueba(){
+    print(tipoValue);
+  }
 
   @override
   ngOnInit() {
     _campusService.getAllCampus().then((lista){
-      print(lista);
       campusList = lista;
+    });
+    _campusService.getTipos().then((lista){
+      tiposList = lista;
     });
   }
 }
