a57192dc698aac0d1e2d3f106c0482478c4c594b
zarisi <isdswii@gmail.com>
2017-02-03 14:31:59 +0100

    Finished Campus main component

--- client/web/lib/component/info/campus/campus_component.dart
+++ client/web/lib/component/info/campus/campus_component.dart
@@ -8,22 +8,85 @@ import 'package:aod3/service/campus_service.dart';
     providers: const [CampusService])
 class CampusComponent implements OnInit{
   final CampusService _campusService;
-  CampusComponent(this._campusService);
-  List<Map> campusList;
+  List<Map> campusList = new List<Map>();
   List<Map> tiposList;
-  int tipoValue;
+  List<Map> ponentesList;
+  List<Map> etiquetasList;
+  List<Map> eventosList;
+  List<Map> formatosList;
+  int _tipoValue;
+  String _ponenteValue;
+  int _etiquetaValue;
+  int _eventoValue;
+  int _formatoValue;
 
-  prueba(){
-    print(tipoValue);
+  CampusComponent(this._campusService);
+
+  void reprintCampusList() {
+    _campusService.getAllCampus(tipo: _tipoValue, aparece: _ponenteValue,
+        tema: _etiquetaValue, evento: _eventoValue, formato: _formatoValue).then((lista){
+      campusList = lista;
+    });
   }
 
   @override
   ngOnInit() {
     _campusService.getAllCampus().then((lista){
-      campusList = lista;
+      campusList=lista;
     });
     _campusService.getTipos().then((lista){
       tiposList = lista;
     });
+    _campusService.getEventos().then((lista){
+      eventosList = lista;
+    });
+    _campusService.getEtiquetas().then((lista){
+      etiquetasList = lista;
+    });
+    _campusService.getFormatos().then((lista){
+      formatosList = lista;
+    });
+    _campusService.getPonentes().then((lista){
+      ponentesList = lista;
+    });
+  }
+
+  int get tipoValue => _tipoValue;
+
+  set tipoValue(int value) {
+    _tipoValue = value;
+    reprintCampusList();
+  }
+
+  String get ponenteValue => _ponenteValue;
+
+  set ponenteValue(String value) {
+    if (value == 'null'){
+      _ponenteValue = null;
+    } else {
+      _ponenteValue = value;
+    }
+    reprintCampusList();
+  }
+
+  int get formatoValue => _formatoValue;
+
+  set formatoValue(int value) {
+    _formatoValue = value;
+    reprintCampusList();
+  }
+
+  int get eventoValue => _eventoValue;
+
+  set eventoValue(int value) {
+    _eventoValue = value;
+    reprintCampusList();
+  }
+
+  int get etiquetaValue => _etiquetaValue;
+
+  set etiquetaValue(int value) {
+    _etiquetaValue = value;
+    reprintCampusList();
   }
 }
--- client/web/lib/service/campus_service.dart
+++ client/web/lib/service/campus_service.dart
@@ -2,29 +2,56 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:html';
 import 'package:angular2/core.dart';
+import 'package:fluri/fluri.dart';
 
 @Injectable()
 class CampusService {
-  static const String urlBase = "http://localhost:8756/campus";
-  static const String urlContenido = "/contenido";
-  static const String urlTipo = "/tipo";
+  Uri urlBase = new Uri(
+      scheme: 'http', host: 'localhost', port: 8756, pathSegments: ['campus']);
+  static const String urlContenido = "contenido";
+  static const String urlTipo = "tipo";
+  static const String urlEvento = "evento";
+  static const String urlEtiqueta = "tema";
+  static const String urlFormato = "formato";
+  static const String urlPonente = "ponente";
 
+  Future<List<Map>> getAllCampus(
+      {int offset: 0,
+      int limit: 9,
+      int formato,
+      int evento,
+      int plataforma,
+      String aparece,
+      int tipo,
+      int tema}) async {
+    Fluri url = new Fluri.fromUri(urlBase);
 
-  Future<List<Map>> getAllCampus({int offset: 0,
-  int limit: 9,
-  int formato,
-  int evento,
-  int plataforma,
-  String aparece,
-  int tema}) async {
-    String conditions = "?limit=$limit&offset=$offset";
-    if(formato!=null) conditions += "&$formato";
-    if(evento!=null) conditions += "&$evento";
-    if(plataforma!=null) conditions += "&$plataforma";
-    if(aparece!=null) conditions += "&$aparece";
-    if(tema!=null) conditions += "&$tema";
+    url.addPathSegment("contenido");
+    url.setQueryParam("limit", limit.toString());
+    url.setQueryParam("offset", offset.toString());
 
-    return HttpRequest.request(urlBase + urlContenido + conditions).then((HttpRequest result) {
+    if (formato != null) url.setQueryParam("formato", formato.toString());
+    if (tipo != null) url.setQueryParam("tipo", tipo.toString());
+    if (evento != null) url.setQueryParam("evento", evento.toString());
+    if (plataforma != null)
+      url.setQueryParam("plataforma", plataforma.toString());
+    if (aparece != null) url.setQueryParam("aparece", aparece);
+    if (tema != null) url.setQueryParam("tema", tema.toString());
+
+    return HttpRequest.request(url.toString()).then((HttpRequest result) {
+      return JSON.decode(result.response);
+    })
+      ..catchError((Event error) {
+        print("ERROR: couldnt get a response from the server: ${error.type}");
+        return null;
+      });
+  }
+
+  Future<List<Map>> getTipos() async {
+    Fluri url = new Fluri.fromUri(urlBase);
+    url.addPathSegment(urlTipo);
+
+    return HttpRequest.request(url.toString()).then((HttpRequest result) {
       return JSON.decode(result.response);
     })
       ..catchError((error) {
@@ -33,8 +60,50 @@ class CampusService {
       });
   }
 
-  Future<List<Map>> getTipos(){
-    return HttpRequest.request(urlBase + urlTipo).then((HttpRequest result) {
+  Future<List<Map>> getEventos() async {
+    Fluri url = new Fluri.fromUri(urlBase);
+    url.addPathSegment(urlEvento);
+
+    return HttpRequest.request(url.toString()).then((HttpRequest result) {
+      return JSON.decode(result.response);
+    })
+      ..catchError((error) {
+        print("ERROR: couldnt get a response from the server: $error");
+        return null;
+      });
+  }
+
+  Future<List<Map>> getEtiquetas() async {
+    Fluri url = new Fluri.fromUri(urlBase);
+    url.addPathSegment(urlEtiqueta);
+
+    return HttpRequest.request(url.toString()).then((HttpRequest result) {
+      return JSON.decode(result.response);
+    })
+      ..catchError((error) {
+        print("ERROR: couldnt get a response from the server: $error");
+        return null;
+      });
+  }
+
+  Future<List<Map>> getFormatos() async {
+    Fluri url = new Fluri.fromUri(urlBase);
+    url.addPathSegment(urlFormato);
+
+    return HttpRequest.request(url.toString()).then((HttpRequest result) {
+      return JSON.decode(result.response);
+    })
+      ..catchError((error) {
+        print("ERROR: couldnt get a response from the server: $error");
+        return null;
+      });
+  }
+
+  Future<List<Map>> getPonentes() async {
+    Fluri url = new Fluri.fromUri(urlBase);
+    url.addPathSegment(urlPonente);
+
+    return HttpRequest.request(url.toString()).then((HttpRequest result) {
       return JSON.decode(result.response);
     })
       ..catchError((error) {
