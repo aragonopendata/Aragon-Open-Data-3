6e3dbb68408bc56d9c1b5df6ab60c0cf69b6349f
zarisi <isdswii@gmail.com>
2017-02-27 16:28:05 +0100

    fixed tests

--- lib/service/info_service.dart
+++ lib/service/info_service.dart
@@ -24,11 +24,6 @@ class InfoService {
             '/portal/social-data',
             '/public/i/aplicaciones/logoAppOpenSocialData.png'),
         new Aplicacion(
-            'BOA Boletín Oficial de Aragón',
-            'El Boletín Oficial de Aragón (BOA) es el diario oficial de la Comunidad Autónoma de Aragón, a través del cual se hacen públicos los documentos que, en virtud de los principios constitucionales de publicidad de las normas y de seguridad jurídica, deben ser objeto de publicidad oficial. Ahora puedes consultarlo en abierto.',
-            'http://www.boa.aragon.es/EBOA/opendata.htm',
-            '/public/i/aplicaciones/logoAppBOA.png'),
-        new Aplicacion(
             'Mapa de los Bienes Culturales de Aragón',
             'El mapa con los Bienes Culturales de Aragón forma parte del proyecto Aragón en la mano, que está siendo desarrollado, en el área de Ciencias Sociales, por los alumnos de tercer curso de Educación Secundaria Obligatoria del colegio Santa María del Pilar de Zaragoza.',
             'http://www.arcgis.com/apps/StorytellingTextLegend/index.html?appid=3f1d252c1db64140817ab84f0a03524c',
@@ -38,11 +33,6 @@ class InfoService {
             'Una herramienta que permite visualizar cuánto dinero gastan la Administración General del Estado y la Seguridad Social, y cómo lo gastan. Descubre fácilmente de dónde vienen los ingresos y localiza rápidamente cualquier partida de gasto con el buscador. Sí, la pregunta del millón tiene respuesta.',
             'http://dondevanmisimpuestos.es/',
             '/public/i/aplicaciones/dvmi.png'),
-        new Aplicacion(
-            'ENCUENTRALO ZgZ',
-            'La Aplicación móvil gratuita de la ciudad de Zaragoza, donde los usuarios podrán encontrar cualquier tipo de servicio, información, comercio o lugar de interés, en tan solo unos segundos, y donde los comercios pueden publicitarse en más de 500.000 smartphones. Disponible en Google Play y App Store',
-            'https://play.google.com/store/apps/details?id=com.adianteventures.adianteapps.encuentralozgz&hl=es',
-            '/public/i/aplicaciones/ENCUENTRALOZgZtxiki.png'),
         new Aplicacion(
             'eBolets Las setas de Aragón en tu móvil!',
             'App de predicción micológica diaria fiable para las especies más apreciadas de la comunidad de Aragón. App con precisión en la geolocalización de las setas hasta 250 m x 250 m con la ayuda de mapas topográficos y realidad aumentada.',
@@ -59,14 +49,9 @@ class InfoService {
             'http://laboratorio.diariodenavarra.es/jacathon/',
             '/public/i/jacathon/Picto03_Poblacion_Escolar_Jacathon.jpg'),
         new Aplicacion(
-            'Aragón Open Trails',
-            'Equipo <a href="https://github.com/aragonopendata/Huracan">Huracán</a>: Carlos Matallín Civera, Gonzalo Izquierdo Gracia, Luís Bosque y Sergio Rubio Gracia. Descubre los senderos de Aragón con esta aplicación. Encuentra además los senderos más cercanos a tu ubicación y recomendaciones cercanas a los senderos.',
-            'http://aot.rbel.co/',
-            '/public/i/jacathon/Picto04_AragonOpenTrails_Jacathon.jpg'),
-        new Aplicacion(
             'CRAs Aragón',
             'Equipo <a href="https://github.com/aragonopendata/JodoCoders">JodoCoders</a>: Javier Rubio Rubio, Dani Latorre Velilla, Jesús Barón Rodríguez y Rafael Ramos Guallar. Los Colegios Rurales Agrupados son los centros donde se educan muchos niños de Aragón, descubre los movimientos que estos niños tienes que hacer para acudir a clase.',
-            'http://crasaragon.com/',
+            'http://opendata.aragon.es/apps/cras',
             '/public/i/jacathon/Picto05_CRAs_Aragon_Jacathon.jpg'),
         new Aplicacion(
             '¿Conoces Aragón?',
